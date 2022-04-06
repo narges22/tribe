cebbfbec1a9c6ed34a337e4db43069614f9174fd
narges22 <narges_shaker@ymail.com>
2022-04-06 17:10:14 +0430

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as TribeProvider } from "@tribeplatform/react-sdk";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TribeProvider
+      config={{
+        accessToken:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkdVRVNUX0dycGVKdlh4M3E3a05zTSIsIm5ldHdvcmtJZCI6IlN5WENCNWpVTzQiLCJuZXR3b3JrRG9tYWluIjoibmFyZ2VzLnRyaWJlcGxhdGZvcm0uY29tIiwidG9rZW5UeXBlIjoiR1VFU1QiLCJlbnRpdHlJZCI6bnVsbCwicGVybWlzc2lvbkNvbnRleHQiOm51bGwsInBlcm1pc3Npb25zIjpudWxsLCJpYXQiOjE2NDg5Njk4MzEsImV4cCI6MTY1MTU2MTgzMX0.k6tYGEJmLO-FI51AJhCftTKZlyo8xSE3G6HT-aScIVY",
+        baseUrl: "https://app.tribe.so/graphql",
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </TribeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
