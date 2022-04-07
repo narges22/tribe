f8adaaa94e40b03ccb713d9977dbbf5f7bc26811
narges22 <narges_shaker@ymail.com>
2022-04-07 11:19:37 +0430

    ADD: loading component

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div className="flex  justify-center justify-items-center py-14">
+      <img src="/images/Hourglass.gif" alt="loading" />
+    </div>
+  );
+};
+
+export default Home;
