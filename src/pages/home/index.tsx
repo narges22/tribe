5f0396a8d0dce33f97c49423dbd8a09f291b1e95
narges22 <narges_shaker@ymail.com>
2022-04-07 14:57:04 +0430

    chore: some changes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,4 @@
-import { TribeClient } from "@tribeplatform/gql-client";
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import Dialog from "../../components/dialog";
 import Spaces from "../../components/Spaces";
 
@@ -8,7 +7,7 @@ const Home: FC = () => {
     <>
       <main>
         <div className="lg:w-3/4 m-auto flex justify-between p-3">
-          <div className="flex-1 flex justify-start font-bold text-xl">
+          <div className="flex-1 text-cyan-900 flex justify-start font-bold text-xl">
             Spaces
           </div>
           <div className="flex-1 flex justify-end">
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,7 +3,8 @@ import Posts from "../../components/posts";
 
 const PostList: FC = () => {
   return (
-    <div>
+    <div className="lg:w-3/4 m-auto p-5">
+      <div className="text-center text-cyan-900 font-bold text-xl">Posts</div>
       <Posts />
     </div>
   );
